a6fff8c573981592e961d3fdf956966e6ed0cf84
CodeFlow / <k1ri86mail@gmail.com>
2024-05-31 01:07:26 +0200

    transiciones-page-com

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import CoverParticles from "@/components/cover-particles";
+import CoverParticles from "@/components/particulas";
+import TransicionPage from "@/components/transiciones-page";
 
 export default function Home() {
   return (
     <main>
+      <TransicionPage />
       <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
         <CoverParticles />
-        <p>CodeFlow</p>
-        <p>Portfolio</p>
+        <p>CodeFlow | Portfolio</p>
       </div>
     </main>
   );
